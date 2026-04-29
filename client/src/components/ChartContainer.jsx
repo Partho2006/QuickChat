dad36fad59730571779d61c5db209cac07247416
Partho2006 <parthojotyroychowdhury@gmail.com>
2026-04-29 16:33:39 +0530

    right side bar

--- client/src/components/ChartContainer.jsx
+++ client/src/components/ChartContainer.jsx
@@ -5,16 +5,14 @@ import { formatMessageTime } from '../lib/utils';
 
 const ChartContainer = ({ selectedUser, setSelectedUser }) => {
   const scrollEnd = useRef();
-  useEffect(()=> {
-    if (scrollEnd.current) {
-      scrollEnd.current.scrollIntoView({ behavior: 'smooth' });
-    }
-  },[])
+  useEffect(() => {
+    scrollEnd.current?.scrollIntoView({ behavior: 'smooth' });
+  }, [messagesDummyData, selectedUser])
 
   return selectedUser ? (
-    <div className="h-full overflow-scroll relative backdrop-blur-lg">
+    <div className="h-full overflow-y-scroll relative backdrop-blur-lg">
       {/* Header */}
-      <div className='flex itemss-center gap-3 py-3 mx-4 border-b border-stone-300'>
+      <div className='flex items-center gap-3 py-3 mx-4 border-b border-stone-300'>
         <img src={assets.profile_martin} alt="" className='w-8 rounded-full' />
         <p className='flex-1 text-lg text-white flex items-center gap-2'>
           Martin Johnson
@@ -24,9 +22,9 @@ const ChartContainer = ({ selectedUser, setSelectedUser }) => {
         <img src={assets.help_icon} alt="" className='max-md:hidden max-w-7' />
       </div>
 
-    {/*  Chat Area */}
+      {/*  Chat Area */}
       <div className="flex flex-col h-[calc(100%-120px)] overflow-y-scroll p-3 pb-6">
-        {messagesDummyData.map((msg, index)=> (
+        {messagesDummyData.map((msg, index) => (
           <div key={index} className={`flex items-end gap-2 justify-end ${msg.senderId !== '680f50e4f10f3cd28382ecf9' && 'flex-row-reverse'}`}>
             {msg.image ? (
               <img src={msg.image} alt="" className='max-w-[230px] border border-gray-700 rounded-lg overflow-hidden mb-8' />
@@ -41,6 +39,18 @@ const ChartContainer = ({ selectedUser, setSelectedUser }) => {
         ))}
 
         <div className="" ref={scrollEnd}></div>
+      </div>
+
+      {/* Bottom Area */}
+      <div className="absolute bottom-0 left-0 right-0 flex items-center gap-3 p-3">
+        <div className="flex-1 flex items-center bg-gray-100/12 px-3 rounded-full">
+          <input type="text" placeholder='Send a message' className='flex-1 text-smm p-3 border-none rounded-lg outline-none text-white placeholder-gray-400' />
+          <input type="file" name="" id="image" accept='image/png, image/jpeg' hidden />
+          <label htmlFor="image">
+            <img src={assets.gallery_icon} alt="" className='w-5 mr-2 cursor-pointer' />
+          </label>
+        </div>
+        <img src={assets.send_button} alt="" className='w-12 cursor-poiter' />
       </div>
     </div>
   ) : (
