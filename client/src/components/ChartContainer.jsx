688d0440e423d01e7df477a4757cd8471fd4b11c
Partho2006 <parthojotyroychowdhury@gmail.com>
2026-05-20 20:21:23 +0530

    modifying chatContainer

--- client/src/components/ChartContainer.jsx
+++ client/src/components/ChartContainer.jsx
@@ -14,10 +14,6 @@ const ChartContainer = () => {
 
   const [input, setInput] = useState('');
 
-  useEffect(() => {
-    scrollEnd.current?.scrollIntoView({ behavior: 'smooth' });
-  }, [messagesDummyData, selectedUser]);
-
   const handleSendMessage = async (e)=> {
     e.preventDefault();
     if (input.trim() === "") return null;
@@ -40,14 +36,26 @@ const ChartContainer = () => {
     reader.readAsDataURL(file);
   }
 
+  useEffect(()=> {
+    if (selectedUser) {
+      getMessages(selectedUser._id);
+    }
+  }, [selectedUser])
+
+  useEffect(() => {
+    if (scrollEnd.current && messages) {
+    scrollEnd.current?.scrollIntoView({ behavior: 'smooth' });
+    }
+  }, [messages]);
+
   return selectedUser ? (
     <div className="h-full overflow-y-scroll relative backdrop-blur-lg">
       {/* Header */}
       <div className='flex items-center gap-3 py-3 mx-4 border-b border-stone-300'>
-        <img src={assets.profile_martin} alt="" className='w-8 rounded-full' />
+        <img src={selectedUser.profilePic || assets.avatar_icon} alt="" className='w-8 rounded-full' />
         <p className='flex-1 text-lg text-white flex items-center gap-2'>
-          Martin Johnson
-          <span className='w-2 h-2 rounded-full bg-green-500'></span>
+          {selectedUser.fullName}
+          {onlineUsers.includes(selectedUser._id)}<span className='w-2 h-2 rounded-full bg-green-500'></span>
         </p>
         <img onClick={() => setSelectedUser(null)} src={assets.arrow_icon} alt="" className='md:hidden max-w-7' />
         <img src={assets.help_icon} alt="" className='max-md:hidden max-w-7' />
@@ -55,15 +63,15 @@ const ChartContainer = () => {
 
       {/*  Chat Area */}
       <div className="flex flex-col h-[calc(100%-120px)] overflow-y-scroll p-3 pb-6">
-        {messagesDummyData.map((msg, index) => (
-          <div key={index} className={`flex items-end gap-2 justify-end ${msg.senderId !== '680f50e4f10f3cd28382ecf9' && 'flex-row-reverse'}`}>
+        {messages.map((msg, index) => (
+          <div key={index} className={`flex items-end gap-2 justify-end ${msg.senderId !== authUser._id && 'flex-row-reverse'}`}>
             {msg.image ? (
               <img src={msg.image} alt="" className='max-w-[230px] border border-gray-700 rounded-lg overflow-hidden mb-8' />
             ) : (
-              <p className={`p-2 max-w-[200px] md:text-sm font-light rounded-lg mb-8 break-all bg-violet-500/30 text-white ${msg.senderId === '680f50e4f10f3cd28382ecf9' ? 'rounded-br-none' : 'rounded-bl-none'}`}>{msg.text}</p>
+              <p className={`p-2 max-w-[200px] md:text-sm font-light rounded-lg mb-8 break-all bg-violet-500/30 text-white ${msg.senderId === authUser._id ? 'rounded-br-none' : 'rounded-bl-none'}`}>{msg.text}</p>
             )}
             <div className="text-center text-xs">
-              <img src={msg.senderId === '680f50e4f10f3cd28382ecf9' ? assets.avatar_icon : assets.profile_martin} alt="" className='w-7 rounded-full' />
+              <img src={msg.senderId === authUser._id ? authUser._id?.profilePic || assets.avatar_icon : selectedUser?.profilePic ||  assets.profile_martin} alt="" className='w-7 rounded-full' />
               <p className="text-gray-500">{formatMessageTime(msg.createdAt)}</p>
             </div>
           </div>
